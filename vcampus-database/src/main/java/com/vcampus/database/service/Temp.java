f7db04a5ac2fef91ea439a0f688d71bfa982a5ec
lee23james <2059825970@qq.com>
2025-09-03 18:03:57 +0800

    加上了服务端和数据端怎么建立联系，包括依赖添加，同时怎么处理

---
+++ vcampus-database/src/main/java/com/vcampus/database/service/Temp.java
@@ -0,0 +1,7 @@
+package com.vcampus.database.service;
+
+public class Temp {
+    public void function() {
+        System.out.println("i am pig");
+    }
+}
---
+++ vcampus-server/src/main/java/com/vcampus/server/service/Temp1.java
@@ -0,0 +1,11 @@
+package com.vcampus.server.service;
+
+import com.vcampus.database.service.Temp;
+
+public class Temp1 {
+    public void function1() {
+        Temp temp = new Temp();
+        temp.function();
+    }
+}
+
